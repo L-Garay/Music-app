54405a460b2cc954f530624a297d4229ce9aedb6
L-Garay <garay.logan@gmail.com>
2019-11-29 18:38:48 -0700

    friday commit

--- app/Models/Song.js
+++ app/Models/Song.js
@@ -11,50 +11,56 @@ export default class Song {
 
   get Template() {
     return `
-    <div class="card mb-3" style="max-width: 550px;">
-                <div class="row no-gutters">
-                  <div class="col-md-4">
-                    <img src="${this.albumArt}" height="150" width="150" alt="jlkdjf">
-                  </div>
-                  <div class="col-md-8">
-                    <div class="card-body">
-                      <h5 class="card-title">${this.title}</h5>
-                      <p class="card-text">${this.artist}</p>
-                      <p class="card-text">${this.album}</p>
-                      <p class="card-text"><small class="text-muted">Price: ${this.price}</small></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            <div>
-              <audio src="${this.preview}" controls></audio>
-              <button type="button" class="btn" onclick="app.songsController.addSong('${this._id}')">Add Song</button>
-            </div>
+    <div class="card mb-3 w-100 d-flex">
+    <div class="row no-gutters">
+      <div class="col-3 align-self-center">
+        <img src="${this.albumArt}" height="75" width="75" alt="fdgs">
+      </div>
+      <div class="col-9">
+        <div class="card-body d-flex justify-content-center flex-column">
+          <p class="card-title mb-0"><b>${this.title}</b></p>
+          <p class="card-text font1 mb-0">${this.artist} | ${this.album}</p>
+          <p class="card-text font2 mb-0">${this.price}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div>
+    <audio class="audio" src="${this.preview}" controls></audio>
+    <button type="button" class="btn" onclick="app.songsController.addSong('${this._id}')">Add Song</button></div>
 
         `;
   }
 
   get playlistTemplate() {
     return `
-    <div class="card mb-3 w-100">
-                <div class="row no-gutters">
-                  <div class="col-md-2 justify-content-center justify-content-md-start">
-                    <img src="${this.albumArt}" height="100" width="100" alt="fdgs">
-                  </div>
-                  <div class="col-md-10">
-                    <div class="card-body d-flex justify-content-center">
-                      <p class="card-title p-3 mb-0"><b>${this.title}</b></p>
-                      <p class="card-text p-3 mb-0">${this.artist}</p>
-                      <p class="card-textm p-3 mb-0">${this.album}</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            <div class="">
-              <audio src="${this.preview}" controls></audio>
-              <button type="button" class="btn" onclick="app.songsController.removeSong('${this._id}')">Remove Song</button>
-            </div>
+    <div class="card mb-3 w-100 d-flex">
+    <div class="row no-gutters">
+      <div class="col-3 align-self-center">
+        <img src="${this.albumArt}" height="75" width="75" alt="fdgs">
+      </div>
+      <div class="col-9">
+        <div class="card-body d-flex justify-content-center flex-column">
+          <p class="card-title mb-0"><b>${this.title}</b></p>
+          <p class="card-text font1 mb-0">${this.artist}</p>
+          <p class="card-text font2 mb-0">${this.album}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div>
+    <audio class="audio" src="${this.preview}" controls></audio>
+    <button type="button" class="btn" onclick="app.songsController.removeSong('${this._id}')">Remove Song</button></div>
 
         `;
+  }
+  get currentTemplate() {
+    return `
+    <img src="${this.albumArt}" height="420" width="420" alt="active music" />
+          <div class="d-flex flex-column align-items-center">
+            <h3>${this.title} - ${this.artist}</h3>
+            <p>Album: ${this.album} | Price: ${this.price} here</p>
+            <audio src="${this.preview}" controls></audio>
+          </div>`;
   }
 }
